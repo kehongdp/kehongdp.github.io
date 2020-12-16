f46fbd85a95804ad324da12f8ea57be5c9960052
lkhdp <kehongdp@gmail.com>
2020-12-16 15:22:26 +0800

    修改

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,7 +9,7 @@ headerIcon.addEventListener("click", function() {
   }
 })
 
-
+// 获取URL ？后的值
 function getQueryString() { 
   let reg = new RegExp("(^|&)" + '?' + "=([^&]*)(&|$)", "i"); 
   let query = window.location.search.substr(1).match(reg); 
@@ -18,39 +18,22 @@ function getQueryString() {
   }
   return null; 
 }
-
 let $query = getQueryString();
 
+// 获取随机数
+let chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+function getRandom() {
+  let res = "";
+  for(let i = 0; i < 6 ; i ++) {
+    let x = Math.ceil(Math.random()*35);
+    let y = Math.ceil(Math.random()*35);
+    res += chars[x] + "-" + chars[y];
+  }
+  return res;
+}
+
 $(document).ready(function(){
-  let $tagsBtnList = document.querySelectorAll(".tags-list-button");
-  for (let i = 0; i < $tagsBtnList.length; i++) {
-    if($tagsBtnList[i].dataset.encode == $query) {
-      $tagsBtnList[i].classList.add("tags-list-active");
-      toggleTags(getQueryString())
-    }
-  }
-
-  let $root = $(".result-archives");
-
-  $(".tags-list-button").each(function(index, el) {
-    $(this).click(function(event) {
-      $(this).addClass('tags-list-active').siblings().removeClass('tags-list-active');
-      $encode = $(this).attr("data-encode");
-      toggleTags($encode)
-    });
-  });
-
-  function toggleTags($data) {
-    if ($data != "") {
-      searchBtnTag($data);
-    } else {
-      $(".result-archives-item-link").removeClass('result-archives-item-link-active');
-    }
-  }
-
   function searchBtnTag($data) {
-    $(".result-archives-item").removeAttr("data-mark", "hide");
-    $(".result-archives-item").removeClass('result-archives-item-active');
     let item = $(".result-archives-item");
 
     for(let i = 0; i < item.length; i++) {
@@ -74,4 +57,78 @@ $(document).ready(function(){
       }
     }
   }
+
+  function toggleTags($data) {
+    $(".result-archives-item").removeAttr("data-mark", "hide");
+    $(".result-archives-item").removeClass('result-archives-item-active');
+    if ($data != "") {
+      searchBtnTag($data);
+    } else {
+      $(".result-archives-item-link").removeClass('result-archives-item-link-active');
+    }
+  }
+
+  // 根据URL Tags 切换列表
+  let $tagsBtnList = document.querySelectorAll(".tags-list-button");
+  for (let i = 0; i < $tagsBtnList.length; i++) {
+    if($tagsBtnList[i].dataset.encode == $query) {
+      $tagsBtnList[i].classList.add("tags-list-active");
+      toggleTags(getQueryString())
+    }
+  }
+
+  // 点击Tags 切换列表
+  let $root = $(".result-archives");
+  $(".tags-list-button").each(function(index, el) {
+    $(this).click(function(event) {
+      $(this).addClass('tags-list-active').siblings().removeClass('tags-list-active');
+      $encode = $(this).attr("data-encode");
+      toggleTags($encode)
+    });
+  });
+
+
+
+  // 设置锚点
+  let anchorList = $(".articles-left-content h2");
+  if (anchorList.length != 0) {
+    document.querySelector(".articles-right").style.display = "block";
+    for(let i = 0; i < anchorList.length; i++) {
+      let link = "<a href='#"+getRandom()+"' class='articles-right-anchor'>"+anchorList.eq(i).text()+"</a>";
+      $(".articles-left-content h2").eq(i).attr({
+        "id": getRandom(),
+        "class": "articles-left-content-h2"
+      });
+      $(".articles-right-anchors").append(link);
+    }
+    
+    // 锚点滚到某点
+    let h2Item = document.querySelectorAll('.articles-left-content-h2');
+    let anchorItem = document.querySelectorAll('.articles-right-anchor');
+    for (let i = 0; i < anchorItem.length; i++) {
+      anchorItem[i].onclick = function() {
+        h2Item[i].scrollIntoView({ block: 'start', behavior: 'smooth', inline: 'center'});
+      };
+    }
+
+    // 目录跟随屏幕
+    $(window).on('scroll', ()=>{
+      let scrollTop = $(window).scrollTop();
+      let marginLeft = $(".articles-container").css('marginLeft');
+
+      if(scrollTop > 540){
+        $(".articles-right").css({
+          position: 'fixed',
+          top: '50px',
+          left: 720 + parseInt(marginLeft)+"px"
+        });
+      } else {
+        $(".articles-right").css({
+          position: 'unset',
+          top: 'unset',
+          left: 'unset'
+        });
+      }
+    })
+  } 
 });
